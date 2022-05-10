1ab57376c487f03efff6f63cbb4a5be595e7f396
Manuel Herdez <herdez@gmail.com>
2022-05-10 09:23:10 -0500

    Random Users Api - implementado

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const URL = 'https://reqres.in/api/users?page=2';
+  const URL_REQRES = 'https://reqres.in/api/users?page=2';
+  const URL_RANDOM_USERS =
+    'https://randomuser.me/api/?results=10';
 
   const [usuarios, setUsuarios] = useState([]);
 
@@ -36,9 +38,9 @@ function App() {
   };
 
   const cargarUsuariosAsynAwait = async () => {
-    const respuesta = await fetch(URL);
-    const { data } = await respuesta.json();
-    setUsuarios(data);
+    const respuesta = await fetch(URL_RANDOM_USERS);
+    const { results } = await respuesta.json();
+    setUsuarios(results);
   };
 
   useEffect(() => {
@@ -50,14 +52,23 @@ function App() {
       <header className='App-header'>
         <h3>Api Rest Profile</h3>
         <ul>
-          {usuarios.map(
+          {/* {usuarios.map(
             ({ id, first_name, last_name, avatar }) => (
               <li key={id}>
                 <img src={avatar} alt={first_name} />
                 {first_name} {last_name}
               </li>
             )
-          )}
+          )} */}
+          {usuarios.map(({ login, name, picture }) => (
+            <li key={login.uuid}>
+              <img
+                src={picture.medium}
+                alt={`${name.first} ${name.last}`}
+              />
+              {name.first} {name.last}
+            </li>
+          ))}
         </ul>
       </header>
     </div>
