2f0f407f85836287e287b04352af22e97ba0bd23
Manuel Herdez <herdez@gmail.com>
2022-05-10 09:04:12 -0500

    tres metodos de cargarUsuarios

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const URL = 'https://reqres.in/api/users?page=2';
+
+  const [usuarios, setUsuarios] = useState([]);
+
+  const usuariosUsuariosLocal = [
+    {
+      id: 1,
+      email: 'juan.peres@reqres.in',
+      first_name: 'Juan',
+      last_name: 'Perez',
+    },
+    {
+      id: 2,
+      email: 'esteban.gomez@reqres.in',
+      first_name: 'Esteban',
+      last_name: 'Gomez',
+    },
+    {
+      id: 3,
+      email: 'santiago.lopez@reqres.in',
+      first_name: 'Santiago',
+      last_name: 'Lopez',
+    },
+  ];
+
+  const cargarUsuariosPromise = () => {
+    fetch(URL)
+      .then((respuesta) => respuesta.json())
+      .then(({ data }) => {
+        setUsuarios(data);
+      });
+  };
+
+  const cargarUsuariosAsynAwait = async () => {
+    const respuesta = await fetch(URL);
+    const { data } = await respuesta.json();
+    setUsuarios(data);
+  };
+
+  useEffect(() => {
+    cargarUsuariosAsynAwait();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h3>Api Rest Profile</h3>
+        <ul>
+          {usuarios.map(
+            ({ id, first_name, last_name, avatar }) => (
+              <li key={id}>
+                <img src={avatar} alt={first_name} />
+                {first_name} {last_name}
+              </li>
+            )
+          )}
+        </ul>
       </header>
     </div>
   );
